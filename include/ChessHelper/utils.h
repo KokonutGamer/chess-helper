1a87f35b0f7c7d56348deeabb5cec4caee2c3568
Gabe Lapingcao <mgultra2018@gmail.com>
2026-05-23 18:43:43 -0700

    Implemented utility mapping functions

--- include/ChessHelper/utils.h
+++ include/ChessHelper/utils.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <functional>
 #include <utility>
 #include <vector>
 
@@ -9,12 +10,34 @@ namespace ChessHelper {
 
 template <typename T> using Optional = std::pair<T, bool>;
 
-template <typename T> static inline Optional<T> value(T&& val) {
+using Mapping = std::function<cv::Vec2i(const cv::Mat &, int, int)>;
+
+template <typename T> static inline Optional<T> value(T &&val) {
   return {val, true};
 }
 
 template <typename T> static inline Optional<T> empty() {
   return std::make_pair<T, bool>(T(), false);
+}
+
+static inline cv::Vec2i topLeft(const cv::Mat &image, int r, int c) {
+  cv::Vec2i mapping = {r, c};
+  return mapping;
+}
+
+static inline cv::Vec2i topRight(const cv::Mat &image, int r, int c) {
+  cv::Vec2i mapping = {r, image.cols - c - 1};
+  return mapping;
+}
+
+static inline cv::Vec2i botLeft(const cv::Mat &image, int r, int c) {
+  cv::Vec2i mapping = {image.rows - r - 1, c};
+  return mapping;
+}
+
+static inline cv::Vec2i botRight(const cv::Mat &image, int r, int c) {
+  cv::Vec2i mapping = {image.rows - r - 1, image.cols - c - 1};
+  return mapping;
 }
 
 /**
