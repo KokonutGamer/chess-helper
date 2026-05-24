cb21fbc8a69af20a200fb8771ce6da9d9d52c696
Gabe Lapingcao <mgultra2018@gmail.com>
2026-05-23 19:53:09 -0700

    Refactored utilities to use Point2i instead of Vec2i

--- include/ChessHelper/utils.h
+++ include/ChessHelper/utils.h
@@ -11,7 +11,7 @@ namespace ChessHelper {
 
 template <typename T> using Optional = std::pair<T, bool>;
 
-using Mapping = std::function<cv::Vec2i(const cv::Mat &, int, int)>;
+using Mapping = std::function<cv::Point2i(const cv::Mat &, int, int)>;
 
 template <typename T> static inline Optional<T> value(T &&val) {
   return {val, true};
@@ -21,23 +21,23 @@ template <typename T> static inline Optional<T> empty() {
   return std::make_pair<T, bool>(T(), false);
 }
 
-static inline cv::Vec2i topLeft(const cv::Mat &image, int r, int c) {
-  cv::Vec2i mapping = {r, c};
+static inline cv::Point2i topLeft(const cv::Mat &image, int r, int c) {
+  cv::Point2i mapping = {r, c};
   return mapping;
 }
 
-static inline cv::Vec2i topRight(const cv::Mat &image, int r, int c) {
-  cv::Vec2i mapping = {r, image.cols - c - 1};
+static inline cv::Point2i topRight(const cv::Mat &image, int r, int c) {
+  cv::Point2i mapping = {r, image.cols - c - 1};
   return mapping;
 }
 
-static inline cv::Vec2i botLeft(const cv::Mat &image, int r, int c) {
-  cv::Vec2i mapping = {image.rows - r - 1, c};
+static inline cv::Point2i botLeft(const cv::Mat &image, int r, int c) {
+  cv::Point2i mapping = {image.rows - r - 1, c};
   return mapping;
 }
 
-static inline cv::Vec2i botRight(const cv::Mat &image, int r, int c) {
-  cv::Vec2i mapping = {image.rows - r - 1, image.cols - c - 1};
+static inline cv::Point2i botRight(const cv::Mat &image, int r, int c) {
+  cv::Point2i mapping = {image.rows - r - 1, image.cols - c - 1};
   return mapping;
 }
 
@@ -56,5 +56,5 @@ static inline int hyperbolic(int r, int c, int targetR, int targetC) {
  * Finds the outer-most corners using the binary mask produced by corner
  * detection.
  */
-Optional<std::vector<cv::Vec2i>> findCorners(cv::Mat &);
+Optional<std::vector<cv::Point2i>> findCorners(cv::Mat &);
 } // namespace ChessHelper
